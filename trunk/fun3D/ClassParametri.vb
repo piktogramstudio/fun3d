874aad9ae3ed81f174c5c340628cc7c62ecbc509
mitrovic.bojan <mitrovic.bojan@dfd97219-2b56-0410-b3d1-17084a493b65>
2009-08-05 14:53:27 +0000

    Final code for objects (raw)

---
+++ trunk/fun3D/ClassParametri.vb
@@ -0,0 +1,54 @@
+Imports System.ComponentModel
+<System.Serializable()> _
+Public Class ClassParametri
+    Private naziv As String = "new1"
+    Private vrednost As Single = 0
+    Dim minS As Single = -10
+    Dim maxS As Single = 10
+    Dim stepS As Single = 1
+    <Category("Value Inspector"), DisplayName("Min value")> _
+    Public Property sliderMinimum() As Single
+        Get
+            Return Me.minS
+        End Get
+        Set(ByVal value As Single)
+            Me.minS = value
+        End Set
+    End Property
+    <Category("Value Inspector"), DisplayName("Max value")> _
+    Public Property sliderMaximum() As Single
+        Get
+            Return Me.maxS
+        End Get
+        Set(ByVal value As Single)
+            Me.maxS = value
+        End Set
+    End Property
+    <Category("Value Inspector"), DisplayName("Step"), Description("Increment and decrement step for Value inspector")> _
+    Public Property sliderStep() As Single
+        Get
+            Return Me.stepS
+        End Get
+        Set(ByVal value As Single)
+            Me.stepS = value
+        End Set
+    End Property
+    <Category("Meta")> _
+    Public Property Name() As String
+        Get
+            Return Me.naziv
+        End Get
+        Set(ByVal value As String)
+            naziv = value
+        End Set
+    End Property
+    <Category("Value")> _
+    Public Property value() As Single
+        Get
+            Return vrednost
+        End Get
+        Set(ByVal value As Single)
+            vrednost = value
+        End Set
+    End Property
+End Class
